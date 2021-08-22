505f348400830f96ab174141096a8c2f61d08f7c
ismaileel <ismail.22120401@gmail.com>
2021-08-22 13:34:08 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga, quibusdam commodi nulla autem ipsum quisquam temporibus esse quaerat alias, sint delectus in cupiditate hic consectetur consequatur architecto, molestiae ex mollitia.</p>
 </body>
 </html>
